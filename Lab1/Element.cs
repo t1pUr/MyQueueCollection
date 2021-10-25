37ebcc6dd2955174d6cf7805c655c4d60793d98b
t1pUr <simplex1135@gmail.com>
2021-10-25 09:49:08 +0300

    Minor changes in Element.cs

--- Lab1/Element.cs
+++ Lab1/Element.cs
@@ -1,19 +1,22 @@
 ﻿namespace Lab1
 {
-  internal class Element<T>
-  {
-    private T data;
-
-    public Element(T data) => this.data = data;
-
-    public Element()
+    internal class Element<T>
     {
-    }
+        private T data;
 
-    public T Data
-    {
-      get => this.data;
-      set => this.data = value;
+        public Element(T data)
+        {
+            this.data = data;
+        }
+            
+
+        public Element() { }
+        
+
+        public T Data
+        {
+            get => this.data;
+            set => this.data = value;
+        }
     }
-  }
 }
