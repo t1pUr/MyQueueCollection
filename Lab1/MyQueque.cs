f6858dadd23c3574b1132818e7a6ae9abeaf066e
t1pUr <simplex1135@gmail.com>
2021-10-25 10:24:00 +0300

    Implement IEnumerable<T> interface and use foreach for collection

--- Lab1/MyQueque.cs
+++ Lab1/MyQueque.cs
@@ -1,26 +1,28 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Lab1
 {
-  internal class MyQueque<T>
-  {
-    private Element<T>[] elements;
-    private int count;
-
-    public MyQueque(int capacity)
+    internal class MyQueque<T> : IEnumerable<T>
     {
-      if (capacity < 0)
-        throw new ArgumentOutOfRangeException();
-      this.elements = new Element<T>[1];
-    }
+        private Element<T>[] elements;
+        private int count;
+
+        public MyQueque(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException();
+            this.elements = new Element<T>[1];
+        }
 
         public MyQueque()
         {
             elements = new Element<T>[1];
         }
 
-    public int Count
+        public int Count
         {
             get
             {
@@ -28,92 +30,92 @@ namespace Lab1
             }
         }
 
-    public void Clear()
-    {
-      elements = new Element<T>[0];
-      count = 0;
-    }
+        public void Clear()
+        {
+            elements = new Element<T>[0];
+            count = 0;
+        }
 
-    public bool Contains(T elem)
-    {
-      bool flag = false;
-      for (int index = 0; index < elements.Length; ++index)
-      {
-        if (Equals(elements[index].Data, elem))
-          flag = true;
-      }
-      return flag;
-    }
+        public bool Contains(T elem)
+        {
+            bool flag = false;
+            for (int index = 0; index < elements.Length; ++index)
+            {
+                if (Equals(elements[index].Data, elem))
+                    flag = true;
+            }
+            return flag;
+        }
 
-    public void CopyTo(T[] array, int arrayIndex)
-    {
-      if (arrayIndex < 0)
-        throw new ArgumentOutOfRangeException();
-      if (array == null)
-        throw new ArgumentNullException();
-      if (count > array.Length - arrayIndex)
-        throw new ArgumentException();
-      for (int index = arrayIndex; index < array.Length; ++index)
-        array[index] = elements[index].Data;
-    }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (count > array.Length - arrayIndex)
+                throw new ArgumentException();
+            for (int index = arrayIndex; index < array.Length; ++index)
+                array[index] = elements[index].Data;
+        }
 
-    public void Enqueue(T item)
-    {
-      Element<T>[] elementArray = new Element<T>[this.count + 1];
-      if (count > 0)
-      {
-        for (int index = 0; index < elements.Length; ++index)
-          elementArray[index] = elements[index];
-      }
-      elementArray[elementArray.Length - 1] = new Element<T>(item);
-      elements = elementArray;
-      count++;
-    }
+        public void Enqueue(T item)
+        {
+            Element<T>[] elementArray = new Element<T>[this.count + 1];
+            if (count > 0)
+            {
+                for (int index = 0; index < elements.Length; ++index)
+                    elementArray[index] = elements[index];
+            }
+            elementArray[elementArray.Length - 1] = new Element<T>(item);
+            elements = elementArray;
+            count++;
+        }
 
-    public T Dequeue()
-    {
-      if (count == 0)
-        throw new InvalidOperationException();
-      T data = elements[0].Data;
-      Element<T>[] elementArray = new Element<T>[count - 1];
-      for (int index = 0; index < elementArray.Length; ++index)
+        public T Dequeue()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T data = elements[0].Data;
+            Element<T>[] elementArray = new Element<T>[count - 1];
+            for (int index = 0; index < elementArray.Length; ++index)
             {
                 elementArray[index] = elements[index + 1];
             }
-        
-      elements = elementArray;
-      count--;
-      return data;
-    }
 
-        public T Peek() 
+            elements = elementArray;
+            count--;
+            return data;
+        }
+
+        public T Peek()
         {
             return elements[0].Data;
         }
 
-    public T[] ToArray()
-    {
-      T[] objArray = new T[count];
-      for (int index = 0; index < objArray.Length; index++)
-        objArray[index] = elements[index].Data;
-      return objArray;
-    }
+        public T[] ToArray()
+        {
+            T[] objArray = new T[count];
+            for (int index = 0; index < objArray.Length; index++)
+                objArray[index] = elements[index].Data;
+            return objArray;
+        }
 
-    public void TrimExcess()
-    {
-        //In this case this method doing nothing
-    }
+        public void TrimExcess()
+        {
+            //In this case this method doing nothing
+        }
 
-    public bool TryDequeue([MaybeNullWhen(false)] out T result)
-    {
-      if (count == 0)
-      {
-        result = default (T);
-        return false;
-      }
-      result = Dequeue();
-      return true;
-    }
+        public bool TryDequeue([MaybeNullWhen(false)] out T result)
+        {
+            if (count == 0)
+            {
+                result = default(T);
+                return false;
+            }
+            result = Dequeue();
+            return true;
+        }
 
         public bool TryPeek([MaybeNullWhen(false)] out T result)
         {
@@ -126,6 +128,18 @@ namespace Lab1
             return true;
         }
 
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return elements[i].Data;
+            }
+            
+        }
 
-  }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return ((IEnumerable)this).GetEnumerator();
+        }
+    }
 }
